e0ce42ad5f62749347b883a14eae06278506e592
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2022-02-22 16:10:18 -0500

    dominator done

--- main.py
+++ main.py
@@ -3,13 +3,44 @@ import json
 import copy
 import sys
 
-def main():
+from basic_block import form_basic_blocks
+from control_flow_graph import *
+
+def find_dominators(cfg):
+    """
+    - cfg: dict(str, BasicBlock)
+    """
+    dom = dict() # str -> set(str)
+    while True:
+        changed = False
+        for vertex, bb in cfg.items():
+            if vertex not in dom:
+                dom[vertex] = list()
+            # find all predecessors' dominators
+            pred_doms = [dom[p] for p in bb.pred if p in dom]
+            common_doms = set.intersection(*pred_doms) if len(pred_doms) > 0 else set()
+            common_doms.add(vertex) # reflexive
+            if not common_doms == dom[vertex]:
+                dom[vertex] = common_doms
+                changed = True
+        if not changed: break
+    return dom
+
+def main(file=None):
     # read from file because it's easier to debug this way
     if file is not None:
         with open(file, "r") as infile:
            prog = json.load(infile)
     else: 
         prog = json.load(sys.stdin)
+
+    for func in prog['functions']:
+        blocks = form_basic_blocks(func['instrs'])
+        blocks = [b for b in blocks if len(b) > 0]
+        cfg = CFG(blocks).cfg
+        dom = find_dominators(cfg)
+        print(dom)
+        
 
 
 if __name__ == "__main__":
@@ -38,4 +69,4 @@ if __name__ == "__main__":
     cse = args.cse
     cf = args.cf
     file = args.filename
-    main(reach, live, const_prop, cse, cf, file)
+    main(file)
